706a60702137519c34621a34a8d40d8028751078
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2023-06-19 18:17:56 +0400

    makeTurn

--- ts.ts
+++ ts.ts
@@ -1,5 +1,32 @@
-type format = number | undefined | null;
-const formatPrice = (a?: format): string => {
-  return a !== undefined && a !== null ? `$ ${a && a.toFixed(2)}` : "$ 0.00";
+type Turtle = "turtle" | null;
+
+type Game = {
+  makeTurn: (direction: "left" | "right") => void;
+  state: Array<Turtle>;
 };
-console.log(formatPrice());
+
+const startGame = (): Game => {
+  const state: Array<Turtle> = ["turtle", null, null, null, null];
+
+  const makeTurn = (direction: "left" | "right") => {
+    if (direction === "right") {
+      if (state[4] != "turtle") {
+        state.pop();
+        state.unshift(null);
+      } else throw new Error("ERROR");
+    } else if (direction === "left") {
+      if (state[0] != "turtle") {
+        state.push();
+        state.shift();
+        return state;
+      } else throw new Error("ERROR");
+    }
+  };
+
+  return { makeTurn, state };
+};
+
+const { makeTurn, state } = startGame();
+makeTurn("right");
+console.log(state);
+export default startGame;
