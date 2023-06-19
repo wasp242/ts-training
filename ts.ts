3cfbddff1b6046e65556862a29794b47d994e0e3
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2023-06-19 15:32:56 +0400

    123

--- ts.ts
+++ ts.ts
@@ -1,12 +1,5 @@
-export function multiplicationTable(size: number): number[][] {
-  let arr: number[][] = [];
-  for (let i: number = 1; i < size + 1; i++) {
-    let innerArr: number[] = [];
-    for (let j: number = i; j < size * i + 1; j += i) {
-      innerArr.push(j);
-    }
-    arr.push(innerArr);
-  }
-  return arr;
-}
-console.log(multiplicationTable(3));
+type format = number | undefined | null;
+const formatPrice = (a?: format): string => {
+  return a !== undefined && a !== null ? `$ ${a && a.toFixed(2)}` : "$ 0.00";
+};
+console.log(formatPrice());
