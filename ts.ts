c9688896c64b0ecc329501aa2f65db04423d784e
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2023-06-21 12:17:51 +0400

    getUserFriends

--- ts.ts
+++ ts.ts
@@ -1,32 +1,31 @@
-type Turtle = "turtle" | null;
-
-type Game = {
-  makeTurn: (direction: "left" | "right") => void;
-  state: Array<Turtle>;
+type User = {
+  id: number;
+  name: string;
+  age: number;
 };
 
-const startGame = (): Game => {
-  const state: Array<Turtle> = ["turtle", null, null, null, null];
+type Friends = [number, number];
 
-  const makeTurn = (direction: "left" | "right") => {
-    if (direction === "right") {
-      if (state[4] != "turtle") {
-        state.pop();
-        state.unshift(null);
-      } else throw new Error("ERROR");
-    } else if (direction === "left") {
-      if (state[0] != "turtle") {
-        state.push();
-        state.shift();
-        return state;
-      } else throw new Error("ERROR");
-    }
-  };
-
-  return { makeTurn, state };
+export type UserResponse = {
+  users: User[];
+  friends: Friends[];
 };
 
-const { makeTurn, state } = startGame();
-makeTurn("right");
-console.log(state);
-export default startGame;
+const userJson = JSON.stringify({
+  users: [
+    { id: 1, name: "John", age: 20 },
+    { id: 2, name: "Mary", age: 21 },
+  ],
+  friends: [[1, 2]],
+});
+
+const getUserFriends = (userJson: string, id: number) => {
+  const { users, friends } = JSON.parse(userJson) as UserResponse;
+  const friendIds = friends
+    .filter((friendPair) => friendPair.includes(id))
+    .flat();
+  return users.filter((user) => user.id !== id && friendIds.includes(user.id));
+};
+console.log(getUserFriends(userJson, 1));
+
+export default getUserFriends;
